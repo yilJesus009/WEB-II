82afda155fa5e082199be9ff78b99d608c5e5292
jesus gil <giljesusgilpantoja@gmail.com>
2026-09-24 22:21:41 -0400

    WEB-II: backend actualizado (index.js, package.json) + blindar .gitignore

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-require('dotenv').config();
-
-const express = require('express');
-const app     = express();
-const port    = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = 3000
+let ejs = require('ejs'); 
 const bodyParser = require('body-parser');
-const db      = require('./models')
-const cors    = require('cors')
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.set('view engine','ejs');
 
-require('./routes')(app);
+app.use(bodyParser.urlencoded({ extended: false}));
 
-db.sequelize.sync({
-    // force:true
-}).then (()=>{
-    console.log('db resync')
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.listen(port,()=>{
-    console.log(`API escuchando en puerto ${port}`)
-});
+app.get('/hello', (req, res)=>{
+    res.sendFile(__dirname + '/hello.html')
+})
+
+app.get('/hello-ejs', (req, res) => {
+  res.render('prueba-ejs',{name:'Jesus', lastname:'gil'})
+})
+
+app.get('/form', (req,res)=>{
+    res.render('form-persona')
+})
+
+app.post('/form-submit', (req,res)=>{
+  const {name, lastname, ciudad} = req.body;
+  res.render('prueba-ejs', {name, lastname, ciudad})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
